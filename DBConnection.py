252f835680895a9eefbdb1fe6b22c84bb09bf1f0
Enekoiza <eneko.izaguirre@outlook.com>
2022-04-28 14:14:30 +0100

    Removed unused libraries

--- DBConnection.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-def databaseConnection():
-  conn = mysql.connector.connect(user='root', password='argider_12',
-                              host='127.0.0.1', port=3306, database='EASYPEASY',
-                              auth_plugin='mysql_native_password')
-
-  return conn
